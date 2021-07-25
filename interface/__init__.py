737a9fa33c83ce098570dd86b9a03212b1ca5404
Phil Robinson <robinsph@umich.edu>
2021-07-24 23:46:26 -0700

    db location correction

--- interface/__init__.py
+++ interface/__init__.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e4fd2314b7d8e52744874483bb147046'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database_prod.db' 
 db = SQLAlchemy(app)
 
 from interface import routes
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0',debug=True)
